1e1ad6a423e1e245224158bb85a70909186c4b0a
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2025-02-10 06:09:25 +0200

    update

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar = () => {
               <Link href="/log" className="md:ml-10 border-2 border-solid border-orange-500 p-1 text-center w-32 rounded-full transition text-orange-500 hover:bg-orange-500 hover:text-white " onClick={() => setMenuOpen(false)}>LogIn</Link>
               <Link href="/sign" className="border-2 border-solid border-orange-500 p-1 text-center w-32 rounded-full transition  hover:bg-orange-500 hover:text-white" onClick={() => setMenuOpen(false)}>SignUp</Link>
               <Link href="/log/admin">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-6 h-6 text-orange-500 transform transition duration-300 hover:scale-150 animate-bounceLight">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-6 h-6 text-orange-500 transform transition duration-300 hover:scale-150 animate-bounceLight" onClick={() => setMenuOpen(false)}>
               <path  fill="currentColor" d="M313.6 304c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 304 0 364.2 0 438.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-25.6c0-74.2-60.2-134.4-134.4-134.4zM400 464H48v-25.6c0-47.6 38.8-86.4 86.4-86.4 14.6 0 38.3 16 89.6 16 51.7 0 74.9-16 89.6-16 47.6 0 86.4 38.8 86.4 86.4V464zM224 288c79.5 0 144-64.5 144-144S303.5 0 224 0 80 64.5 80 144s64.5 144 144 144zm0-240c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z"/>
               </svg>
               </Link>
